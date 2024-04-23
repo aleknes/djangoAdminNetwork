c5488bd331073666d45ed7bf3c29fcd45ab29492
aleknes <andreas@lekn.es>
2024-04-23 18:33:34 +0200

    Added debug option for template dl in urls.py

--- djangoAdminNetwork/urls.py
+++ djangoAdminNetwork/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from networkProvisioning.views import actions
@@ -22,3 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('actions/', actions),
 ]
+
+
+#AL: Allow file download of template files directly from admin interface in debug mode. Probably bypassing logon
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
